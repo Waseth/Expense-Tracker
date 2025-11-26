3cccf2e8e062f5835b7dac3cafc1bd0956412d6c
Waseth <wasethsapriso@gmail.com>
2025-11-26 23:51:50 +0300

    App fully complete

--- index.js
+++ index.js
@@ -7,99 +7,15 @@ let expenseList = document.querySelector(".expense-list");
 let total = document.getElementById("total");
 let showAllBtn = document.getElementById("show-all");
 
+// Global Array for storage(load saved data)
+let savedExpenses = JSON.parse(localStorage.getItem("expenses")) || [];
+
 let totalAmount = 0;
 
-// Dropdown toggler
+// Dropdown toggle
 function toggleMenu() {
     menu.classList.toggle("show");
     showAllBtn.style.display = "none"
-}
-
-
-function addExpense() {
-    // Get the values of the input fields
-    let expenseInput = expense.value.trim();// .trim() helps eliminate invalid inputs like spaces
-    let amountInput = amount.value.trim();
-    let categorySelect = category.value;
-
-
-    let numberInput = Number(amountInput);//.value gives a string but we want an actual number that's why we put Number()
-
-    if (expense.value.length > 9) {
-        showPopUp("Expense name should not exceed 9 characters!");
-        return;
-    }
-    // Check if input is valid
-    if (!expenseInput || !amountInput || !categorySelect) {
-        showPopUp("Please fill in all the fields!");
-        return;// return in the case is used to stop the rest of the code from running if the above if statement evaluates to true which would mean the is not input in at least one input field
-    }
-    // Check if only a number is the input
-    if (isNaN(numberInput) || numberInput <= 0) {
-        showPopUp("Please enter a valid amount!");
-        return;
-    }
-
-    // Create expense card
-    let expenseCard = document.createElement("div");
-    expenseCard.classList.add("expense-card");
-
-    // Create expense texts(Expense card components)
-    let expenseName = document.createElement("div");
-    expenseName.classList.add("expense-text");
-    expenseName.textContent = expenseInput;
-
-    let expenseAmount = document.createElement("div");
-    expenseAmount.classList.add("expense-text");
-    expenseAmount.textContent = "Ksh " + amountInput;
-
-    let expenseCategory = document.createElement("div");
-    expenseCategory.classList.add("expense-text");
-    expenseCategory.textContent = categorySelect;
-
-    let dateDiv = document.createElement("div");
-    dateDiv.classList.add("expense-text");
-    dateDiv.textContent = new Date().toLocaleDateString();
-
-    // Store card input values onto the card element permanently for filtering and updates.
-    expenseCard.expenseNameStore = expenseInput;
-    expenseCard.categoryStore = categorySelect;
-    expenseCard.amountValueStore = numberInput;
-    expenseCard.dateStore = new Date().toISOString().split("T")[0];
-
-    // Create expense button
-    let expenseBtn = document.createElement("button");
-    expenseBtn.classList.add("delete-btn");
-    expenseBtn.textContent = "DELETE";
-
-    // Append children to card
-    expenseCard.appendChild(expenseName);
-    expenseCard.appendChild(expenseAmount);
-    expenseCard.appendChild(expenseCategory);
-    expenseCard.appendChild(dateDiv);
-    expenseCard.appendChild(expenseBtn);
-
-    // Append card to expense list
-    expenseList.appendChild(expenseCard);
-
-
-    //  Clear inputs
-    expense.value = '';
-    amount.value = '';
-    category.value = '';
-
-    // Update totals(Additonal expenses)
-    totalAmount = totalAmount + expenseCard.amountValueStore;
-    total.textContent = "TOTAL : KSH " + totalAmount.toLocaleString();// .toLocaleString() is used when adding commas to monetary figures for example instead of 12345678, we can have 12,345,678
-
-    // Functionality for 'DELETE' button
-    expenseBtn.onclick = function () {
-        totalAmount = totalAmount - expenseCard.amountValueStore;
-        total.textContent = "TOTAL : KSH " + totalAmount.toLocaleString();
-        expenseList.removeChild(expenseCard);
-    }
-
-
 }
 
 // functionality for showing pop up message if the input fields are not all filled up
@@ -113,6 +29,132 @@ function showPopUp(message) {
     }, 3000)
 };
 
+
+// Create expense card(used for localstorage and new inputs)
+function createExpenseCard(data) {
+    let expenseCard = document.createElement("div");
+    expenseCard.classList.add("expense-card");
+
+    //Store values for filtering
+    expenseCard.expenseNameStore = data.name;
+    expenseCard.amountValueStore = data.amount;
+    expenseCard.categoryStore = data.category;
+    expenseCard.dateStore = data.date;
+
+    // Create expense texts(Expense card components)
+    let nameDiv = document.createElement("div");
+    nameDiv.classList.add("expense-text");
+    nameDiv.textContent = data.name;
+
+    let amountDiv = document.createElement("div");
+    amountDiv.classList.add("expense-text");
+    amountDiv.textContent = "Ksh " + data.amount;
+
+    let categoryDiv = document.createElement("div");
+    categoryDiv.classList.add("expense-text");
+    categoryDiv.textContent = data.category;
+
+    let dateDiv = document.createElement("div");
+    dateDiv.classList.add("expense-text");
+    dateDiv.textContent = new Date(data.date).toLocaleDateString();
+
+
+    // Create expense delete button
+    let expenseBtn = document.createElement("button");
+    expenseBtn.classList.add("delete-btn");
+    expenseBtn.textContent = "DELETE";
+
+    // Append children to card
+    expenseCard.appendChild(nameDiv);
+    expenseCard.appendChild(amountDiv);
+    expenseCard.appendChild(categoryDiv);
+    expenseCard.appendChild(dateDiv);
+    expenseCard.appendChild(expenseBtn);
+
+    // Append card to expense list
+    expenseList.appendChild(expenseCard);
+
+    //Delete button logic
+    expenseBtn.onclick = function () {
+        expenseList.removeChild(expenseCard);
+
+        // remove from localStorage list
+        savedExpenses = savedExpenses.filter(item =>
+            !(item.name === data.name &&
+                item.amount === data.amount &&
+                item.date === data.date
+            )
+        )
+        localStorage.setItem("expenses", JSON.stringify(savedExpenses));
+        recalculateVisibleTotal();
+    }
+
+    return expenseCard;
+}
+
+// Functionality for recalculating totals once a card is deleted
+function recalculateVisibleTotal() {
+    let allCards = document.querySelectorAll(".expense-card");
+    let newTotal = 0;
+
+    allCards.forEach(card => {
+        if (card.style.display !== "none") {
+            newTotal = newTotal + card.amountValueStore;
+        }
+    })
+    total.textContent = "TOTAL : KSH " + newTotal.toLocaleString();
+}
+
+// Adding expense cards
+function addExpense() {
+    // Get the values of the input fields
+    let expenseInput = expense.value.trim();// .trim() helps eliminate invalid inputs like spaces
+    let amountInput = amount.value.trim();
+    let categorySelect = category.value;
+
+    if (!expenseInput || !amountInput || !categorySelect) {
+        showPopUp("Please fill in all the fields!");
+        return;// return in the case is used to stop the rest of the code from running if the above if statement evaluates to true which would mean the is not input in at least one input field
+    }
+
+    if (expenseInput.length > 9) {
+        showPopUp("Expense name should not exceed 9 characters!");
+        return;
+    }
+
+    let numberInput = Number(amountInput);//.value gives a string but we want an actual number that's why we put Number()
+
+    if (isNaN(numberInput) || numberInput <= 0) {
+        showPopUp("Please enter a valid amount!");
+        return;
+    }
+
+    // Create an expense object
+    let expenseData = {
+        name: expenseInput,
+        amount: numberInput,
+        category: categorySelect,
+        date: new Date().toISOString().split("T")[0]
+    };
+
+    // Save to storage
+    savedExpenses.push(expenseData);
+    localStorage.setItem("expenses", JSON.stringify(savedExpenses));
+
+    // Create a card
+    createExpenseCard(expenseData);
+
+    // Update totals(Additonal expenses)
+    totalAmount = totalAmount + numberInput;
+    total.textContent = "TOTAL : KSH " + totalAmount.toLocaleString();// .toLocaleString() is used when adding commas to monetary figures for example instead of 12345678, we can have 12,345,678
+
+    //  Clear inputs
+    expense.value = '';
+    amount.value = '';
+    category.value = '';
+}
+
+
 // Filtering by category
 let allCategoryButtons = document.querySelectorAll(".menu");// Grab the category buttons
 
@@ -123,30 +165,31 @@ function filterByCategory(selectedCategory) {
     let filteredTotal = 0;
     //Loop through the cards
     allCards.forEach((card) => {
-        if (card.categoryStore.toLowerCase() === selectedCategory.toLowerCase()) {
+        if (card.categoryStore.toLowerCase() === selectedCategory.trim().toLowerCase()) {
             card.style.display = "flex";
             filteredTotal = filteredTotal + card.amountValueStore;
         } else {
             card.style.display = "none";
         }
-        total.textContent = "TOTAL : KSH " + filteredTotal.toLocaleString();
+
     });
 
+    total.textContent = "TOTAL : KSH " + filteredTotal.toLocaleString();
     //Display show all button once the filtering has taken place to enable user to go back to the full view of cards and their totals
     showAllBtn.style.display = "block";
 };
 
 // Daily filter
-function dailyFilter(){
+function dailyFilter() {
     let allCards = document.querySelectorAll(".expense-card");
     let today = new Date().toISOString().split("T")[0];
     let filteredTotal = 0;
 
-    allCards.forEach( card =>{
-        if(card.dateStore === today){
+    allCards.forEach(card => {
+        if (card.dateStore === today) {
             card.style.display = "flex";
             filteredTotal = filteredTotal + card.amountValueStore;
-        }else{
+        } else {
             card.style.display = "none";
         }
     })
@@ -156,7 +199,7 @@ function dailyFilter(){
 }
 
 // Weekly filter
-function weeklyFilter(){
+function weeklyFilter() {
     let allCards = document.querySelectorAll(".expense-card");
     let today = new Date();
     let filteredTotal = 0;
@@ -170,10 +213,10 @@ function weeklyFilter(){
             return;
         }
 
-        if(diffDays >=0 && diffDays <= 7){
+        if (diffDays >= 0 && diffDays <= 7) {
             card.style.display = "flex";
             filteredTotal = filteredTotal + card.amountValueStore;
-        }else{
+        } else {
             card.style.display = "none";
         }
     });
@@ -183,7 +226,7 @@ function weeklyFilter(){
 }
 
 // Monthly filter
-function monthlyFilter(){
+function monthlyFilter() {
     let allCards = document.querySelectorAll(".expense-card");
     let today = new Date();
     let filteredTotal = 0;
@@ -191,14 +234,14 @@ function monthlyFilter(){
     allCards.forEach(card => {
         let cardDate = new Date(card.dateStore);
 
-        if(
+        if (
             !isNaN(cardDate) &&
             cardDate.getFullYear() === today.getFullYear() &&
             cardDate.getMonth() === today.getMonth()
-        ){
+        ) {
             card.style.display = "flex";
             filteredTotal = filteredTotal + card.amountValueStore;
-        }else{
+        } else {
             card.style.display = "none";
         }
     })
@@ -230,4 +273,19 @@ allCategoryButtons.forEach((clickedBtn) => {
     });
 });
 
+// function to load all cards when the page is opened
+
+function loadSavedExpenses() {
+    savedExpenses.forEach(data => {
+        createExpenseCard(data);
+    });
+
+    // calculate total on load
+    let totalAll = 0;
+    savedExpenses.forEach(item => totalAll = totalAll + item.amount);
+    totalAmount = totalAll;
+    total.textContent = "TOTAL : KSH " + totalAll.toLocaleString();
+}
+
+loadSavedExpenses();
 
